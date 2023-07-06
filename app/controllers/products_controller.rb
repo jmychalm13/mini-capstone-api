ca0d37a78730d1c1163db02139b4b8002fb46209
“Jessica <jmychalm13@gmail.com>
2023-07-05 19:47:58 -0500

    added two more working routes to display first and last product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,14 @@ class ProductsController < ApplicationController
     products = Product.all
     render json: products.as_json
   end
+
+  def first_product
+    product = Product.first
+    render json: product.as_json
+  end
+
+  def last_product
+    product = Product.last
+    render json: product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get "/all_products" => "products#all_products"
+
+  get "/first_product" => "products#first_product"
+
+  get "/last_product" => "products#last_product"
 end
