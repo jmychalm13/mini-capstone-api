281ea6eec79c40a78c1505196817e66f4a5118df
“Jessica <jmychalm13@gmail.com>
2023-07-13 20:39:44 -0500

    create route and index action for Suppliers

---
+++ test/controllers/suppliers_controller_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class SuppliersControllerTest < ActionDispatch::IntegrationTest
+  test "index" do
+    get "/suppliers.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Supplier.count, data.length
+  end
+end
