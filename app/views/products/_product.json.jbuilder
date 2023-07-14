ca245682a56f829102da820570ba8aa1814f3789
“Jessica <jmychalm13@gmail.com>
2023-07-14 17:46:27 -0500

    added working index route and controller for Supplier

--- app/views/products/_product.json.jbuilder
+++ app/views/products/_product.json.jbuilder
@@ -5,3 +5,5 @@ json.image_url product.image_url
 json.is_discounted? product.is_discounted?
 json.tax product.tax
 json.id product.id
+json.supplier_id product.supplier_id
+json.supplier product.supplier
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -14,7 +14,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response 200
 
     data = JSON.parse(response.body)
-    assert_equal ["name", "description", "price", "image_url", "is_discounted?", "tax", "id"], data.keys
+    assert_equal ["name", "description", "price", "image_url", "is_discounted?", "tax", "id", "supplier_id", "supplier"], data.keys
   end
 
   test "create" do
