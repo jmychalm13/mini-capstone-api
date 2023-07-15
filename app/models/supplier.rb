c9c069883a0cbd8924ff3f7dbff8f3370d6cd450
“Jessica <jmychalm13@gmail.com>
2023-07-15 10:22:18 -0500

    added phone number validation for Supplier

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -4,6 +4,7 @@ class Supplier < ApplicationRecord
             uniqueness: { case_sensitive: false },
             length: { minimum: 4, maximum: 254 }
   validates :name, :email, :phone_number, presence: true
+  validates :phone_number, length: { minimum: 10 }
 
   def big_box
     if @name == "Amazon"
