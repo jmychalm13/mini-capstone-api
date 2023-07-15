626f4a05336414c957b521b0839b978dbd0afeeb
“Jessica <jmychalm13@gmail.com>
2023-07-15 10:11:48 -0500

    added email validation and presence validation for all fields

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,3 +1,15 @@
 class Supplier < ApplicationRecord
+  validates :email,
+            format: { with: /\A(.+)@(.+)\z/, message: "Email invalid" },
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 4, maximum: 254 }
   validates :name, :email, :phone_number, presence: true
+
+  def big_box
+    if @name == "Amazon"
+      return true
+    else
+      return false
+    end
+  end
 end
