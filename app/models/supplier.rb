5d2fdc6559d0b6a4637f8366fcfd43d84157ec7b
“Jessica <jmychalm13@gmail.com>
2023-07-16 09:39:25 -0500

    removed image_url from products

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -6,4 +6,6 @@ class Supplier < ApplicationRecord
   validates :name, :email, :phone_number, presence: true
   validates :phone_number, length: { minimum: 10 }
   validates :email, :name, :phone_number, uniqueness: true
+
+  has_many :products
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,10 +1,9 @@
-ActiveRecord::Schema[7.0].define(version: 2023_07_14_010437) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_16_143511) do
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.decimal "price", precision: 9, scale: 2
-    t.string "image_url"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
