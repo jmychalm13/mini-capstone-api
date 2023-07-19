71fdbe97ffde02c2be6aa5f323719cdee959c149
“Jessica <jmychalm13@gmail.com>
2023-07-18 21:14:35 -0500

    added create and show routes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,23 @@
 class OrdersController < ApplicationController
+  def create
+    @order = Order.create(
+      user_id: params[:user_id],
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: params[:subtotal],
+      tax: params[:tax],
+      total: params[:total],
+    )
+    if @order.valid?
+      render template: "orders/show"
+    else
+      # sad path -> validations did not pass
+      render json: { errors: @order.errors.full_messages }, status: 422
+    end
+  end
+
+  def show
+    @order = Order.find_by(id: params[:id])
+    render template: "orders/show"
+  end
 end
--- app/views/orders/_order.json.jbuilder
+++ app/views/orders/_order.json.jbuilder
@@ -1,5 +1,4 @@
-# user_id, product_id, quantity, subtotal, tax, total
-
+json.id order.id
 json.user_id order.user_id
 json.product_id order.product_id
 json.quantity order.quantity
