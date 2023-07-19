25b9283c469faab4cfda58b5f7122af30778ae61
“Jessica <jmychalm13@gmail.com>
2023-07-18 19:33:36 -0500

    added purchase model

--- db/schema.rb
+++ db/schema.rb
@@ -1,9 +1,20 @@
-ActiveRecord::Schema[7.0].define(version: 2023_07_17_234513) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_19_003237) do
   enable_extension "plpgsql"
 
   create_table "images", force: :cascade do |t|
     t.string "url"
     t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.integer "quantity"
+    t.decimal "subtotal", precision: 9, scale: 2
+    t.decimal "tax", precision: 9, scale: 2
+    t.decimal "total", precision: 9, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
