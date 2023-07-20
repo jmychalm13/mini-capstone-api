b695774ea24eb25e563817481f42c46ea067a6ec
“Jessica <jmychalm13@gmail.com>
2023-07-20 18:31:07 -0500

    added authorization

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,33 +1,45 @@
 class OrdersController < ApplicationController
   def create
-    product = Product.find_by(id: params[:product_id])
-    # pp product.price
-    subtotal = params[:quantity].to_i * product.price
-    tax = subtotal * 0.09
-    total = tax + subtotal
-    @order = Order.create(
-      user_id: current_user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      subtotal: subtotal,
-      tax: tax,
-      total: total,
-    )
-    if @order.valid?
-      render template: "orders/show"
+    if current_user
+      product = Product.find_by(id: params[:product_id])
+      # pp product.price
+      subtotal = params[:quantity].to_i * product.price
+      tax = subtotal * 0.09
+      total = tax + subtotal
+      @order = Order.create(
+        user_id: current_user.id,
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        subtotal: subtotal,
+        tax: tax,
+        total: total,
+      )
+      if @order.valid?
+        render template: "orders/show"
+      else
+        # sad path -> validations did not pass
+        render json: { errors: @order.errors.full_messages }, status: 422
+      end
     else
-      # sad path -> validations did not pass
-      render json: { errors: @order.errors.full_messages }, status: 422
+      render json: [], status: :unauthorized
     end
   end
 
   def show
-    @order = Order.find_by(id: params[:id], user_id: current_user.id)
-    render template: "orders/show"
+    if current_user
+      @order = Order.find_by(id: params[:id], user_id: current_user.id)
+      render template: "orders/show"
+    else
+      render json: [], status: :unauthorized
+    end
   end
 
   def index
-    @orders = Order.where(user_id: current_user.id)
-    render template: "orders/index"
+    if current_user
+      @orders = Order.where(user_id: current_user.id)
+      render template: "orders/index"
+    else
+      render json: [], status: :unauthorized
+    end
   end
 end
