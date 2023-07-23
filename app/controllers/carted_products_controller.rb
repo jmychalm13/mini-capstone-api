91dbd8384bf1baa7fdb27bc4f0307d898ef0a260
“Jessica <jmychalm13@gmail.com>
2023-07-23 15:58:01 -0500

    added working create action with user provided params

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,4 +1,13 @@
 class CartedProductsController < ApplicationController
   def create
+    @carted_product = CartedProduct.create(
+      user_id: params[:user_id],
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      status: params[:status],
+      order_id: params[:order_id],
+    )
+
+    render template: "carted_products/show"
   end
 end
